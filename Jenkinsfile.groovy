49a486d3d10ce26a1f2b1614014bc4e36c7ebb44
Jerry <jerryhoerig@gmail.com>
2018-09-18 15:09:35 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node {
-  sh(returnStdout: true, script: "cd gittessting")
+  sh(returnStdout: true, script: "cd gittesting")
   sh(returnStdout: true, script: "pwd")
   shortCommit = sh(returnStdout: true, script: "git branch").trim()
 }
